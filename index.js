15c3d273f28b4880ea967aba476603212ffd7502
Avanish Gharat <127854093+Avamax-Q@users.noreply.github.com>
2023-06-02 19:01:31 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
- 
+$(document).ready(function()
+{
+    $('#submit-btn').click(function()
+    {
+        var input = $('#input-field').val();
+        $.ajax({
+            type: 'POST',
+            url: 'http://127.0.0.1:5000/process_data',
+            contentType: 'application/json',
+            data: JSON.stringify({input: input}),
+            success: function(data)
+            {
+                $('#output-field').val(data.output);
+            },
+            error: function(xhr, status, error)
+            {
+                console.error(status + ': ' + error);
+            }
+        });
+    });
+});
+
+
+function testfunc()
+{
+    console.log("This works")
+}
