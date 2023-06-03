47f6edbb1196e95efe07c908533727bd27452aa3
Avanish Gharat <127854093+Avamax-Q@users.noreply.github.com>
2023-06-03 14:06:48 +0530

    v1

--- index.js
+++ index.js
@@ -1,27 +1,64 @@
-$(document).ready(function()
-{
-    $('#submit-btn').click(function()
-    {
+$(document).ready(function() {
+    $('#submit-btn').click(function() {
         var input = $('#input-field').val();
         $.ajax({
             type: 'POST',
-            url: 'http://127.0.0.1:5000/process_data',
+            url: 'http://192.168.43.170:5000/process_data',
             contentType: 'application/json',
             data: JSON.stringify({input: input}),
-            success: function(data)
-            {
+            success: function(data) {
                 $('#output-field').val(data.output);
             },
-            error: function(xhr, status, error)
-            {
+            error: function(xhr, status, error) {
                 console.error(status + ': ' + error);
             }
         });
     });
+    $('#camera-btn').click(function() {
+        input = "cam$$";//$('#input-field').val();
+        $.ajax({
+            type: 'POST',
+            url: 'http://192.168.43.170:5000/process_data',
+            contentType: 'application/json',
+            data: JSON.stringify({input: input}),
+            success: function(data) {
+                $('#output-field').val(data.output);
+            },
+            error: function(xhr, status, error) {
+                console.error(status + ': ' + error);
+            }
+        });
+
+
+
+    });
 });
+
+var uid
+
+var finalUID = document.getElementById("userMainpage")
+function loginFunc()
+{
+
+    var user =document.getElementById("username")
+    uid=user.value
+    console.log(user.value)
+    localStorage.username=uid
+
+
+}
+
+function loadFunc()
+{
+
+    console.log(uid)
+    finalUID.innerHTML=localStorage.username
+
+}
 
 
 function testfunc()
 {
     console.log("This works")
+
 }
